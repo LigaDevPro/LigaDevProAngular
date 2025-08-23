abf38a598b732606dea9d9db4bd1aaae1c0329bd
Pablo <pabloperalta4604@gmail.com>
2025-08-22 23:03:02 -0300

    Agregado de estructura del home y estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { About } from './about/about';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 
 export const routes: Routes = [
   { path: 'about', component: About },
   { path: 'dashboard', component: Dashboard },
   { path: 'login', component: Login },
+  { path: '', component: Home },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/' },
 ];
