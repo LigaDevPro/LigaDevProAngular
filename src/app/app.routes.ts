c241eb07a8b27703cdc2f16d37dbe0daed806cce
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2025-08-22 21:50:59 -0300

    Agregado de estructura del dashboard/Crud y estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { About } from './about/about';
+import { Dashboard } from './dashboard/dashboard';
+
 
 export const routes: Routes = [
   { path: 'about', component: About },
+  { path: 'dashboard', component: Dashboard },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/' },
 ];
