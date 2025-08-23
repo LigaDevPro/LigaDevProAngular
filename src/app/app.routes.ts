c0eaa09b52308bbcb74ae4f9bb2ec860858b1c8a
Rodrigo <rodrigo_rojas2@hotmail.com>
2025-08-22 22:07:37 -0300

    Agregado de estructura del login y estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { About } from './about/about';
 import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
 
 
 export const routes: Routes = [
   { path: 'about', component: About },
   { path: 'dashboard', component: Dashboard },
+  { path: 'login', component: Login },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/' },
 ];
