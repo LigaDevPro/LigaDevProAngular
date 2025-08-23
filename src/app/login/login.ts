9b7a3fd7adfe60d88174a59c1263e53dd62175b6
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2025-08-23 17:04:21 -0300

    Implementación de Formulario Reactivo en el login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('^[a-zA-Z0-9@#]+$') // solo letras, números y @#
+      ]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login válido', this.loginForm.value);
+    } else {
+      console.log('Login inválido');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  // Getters para el HTML
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
 }
