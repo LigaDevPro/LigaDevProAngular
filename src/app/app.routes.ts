c0b734522913f608094fa49adf9c8b914a61a107
miguel <miguel.scaccia1@gmail.com>
2025-08-22 21:33:09 -0300

    Agregado de estructura del about y estilos globales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: About },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
