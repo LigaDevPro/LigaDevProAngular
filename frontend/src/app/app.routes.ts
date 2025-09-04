231962d995bd4dcba836ea57e56bf5a309f7476c
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2025-09-04 20:19:44 -0300

    Modificado de estructura de carpetas y componentes

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './auth/login/login';
+import { Home } from './pages/home/home';
+import { Register } from './auth/register/register';
+import { FormTournament } from './auth/form-tournament/form-tournament';
+
+
+export const routes: Routes = [
+  { path: 'about', component: About },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'form-tournament', component: FormTournament },
+  { path: '', component: Home },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
