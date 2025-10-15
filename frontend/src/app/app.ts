f5a19617f49869f734a9d59fa86be8031fc26117
MiguelSc <112530966+MiguelSc66@users.noreply.github.com>
2025-10-14 22:53:26 -0300

    feat: Agregar componente de notificaciones a las importaciones de aplicaciones

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./shared/navbar/navbar";
-import { Footer } from "./shared/footer/footer";
+import { Navbar } from './shared/navbar/navbar';
+import { Footer } from './shared/footer/footer';
+import { NotificacionesComponent } from './shared/notificaciones/notificaciones';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, NotificacionesComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('LigaDevProAngular');
